2c802ba9f69bef2c67e083db211a742ec90ec28b
Diogo Santos <santos@transformative.ai>
2023-02-22 00:28:48 +0100

    init commit

--- pip_to_conda.sh
+++ pip_to_conda.sh
@@ -9,20 +9,17 @@ echo "Active environment: $(basename $CONDA_PREFIX)"
 # Get the list of packages installed with pip in the conda environment
 pip_packages=$(pip list --format=freeze | grep -v '^\-e' | cut -d = -f 1)
 
-# Install each package with conda and uninstall the pip version if the installation succeeds
+# Install each package with conda
 for package in $pip_packages; do
     # Print the current package being processed
     echo "Processing package: $package"
 
     # Try to install the package with conda
     if conda install -y $package --force-reinstall; then
-        # If the installation succeeds, print a message and uninstall the pip version of the package
+        # If the installation succeeds, print a message
         echo "Successfully installed with conda: $package"
-        pip uninstall -y $package
-        echo "Removed pip package: $package"
      else
         # If the installation fails, print an error message
         echo "Error: conda install failed for package: $package"
-        pip uninstall -y $package
     fi
 done
